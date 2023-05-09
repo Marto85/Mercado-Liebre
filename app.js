9c1e71253c832945cf34638a11e79f5fd379e5a1
Marto85 <104855025+Marto85@users.noreply.github.com>
2023-05-08 22:25:45 -0300

    Actualizo app.js

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.listen(PORT, () => console.log('Servidor corriendo '+ PORT));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
